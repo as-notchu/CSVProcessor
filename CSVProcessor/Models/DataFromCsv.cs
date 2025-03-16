03223c554f872e21bddcbd62f728c3ff7fb33184
as-notchu <arjunok@proton.me>
2025-03-16 14:38:39 +0100

    Add class DataFromCsv.cs

---
+++ CSVProcessor/Models/DataFromCsv.cs
@@ -0,0 +1,10 @@
+namespace CSVProcessor.Models;
+
+public class DataFromCsv
+{
+    public string Title { get; set; }
+    
+    public string Budget { get; set; }
+    
+    public string ReleaseDate { get; set; }
+}
