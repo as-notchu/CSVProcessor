be244ab74872993a8f46bdc196ad021ca0c53027
as-notchu <arjunok@proton.me>
2025-03-23 18:43:11 +0100

    all fields of DTO is required now

--- CSVProcessor/Models/FilmDTO.cs
+++ CSVProcessor/Models/FilmDTO.cs
@@ -1,10 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CSVProcessor.Models;
 
 public class FilmDTO
 {
+    [Required]
     public string Title { get; set; }
-    
+    [Required]
     public string Budget { get; set; }
-    
+    [Required]
     public string ReleaseDate { get; set; }
 }
