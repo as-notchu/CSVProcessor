02b95e7496bb9626da248db79f2eefcebd98113d
as-notchu <arjunok@proton.me>
2025-03-29 14:40:47 +0100

    Operations enum

---
+++ CSVProcessor/Enum/Operations.cs
@@ -0,0 +1,13 @@
+namespace CSVProcessor.Enum;
+
+public enum Operations
+{
+    CreateActor = 0,
+    RemoveActor = 1,
+    UpdateActor = 2,
+    GetActor = 4,
+    CreateFilm = 5,
+    RemoveFilm = 6,
+    UpdateFilm = 7,
+    GetFilm = 8,
+}
